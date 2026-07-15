0ad1b33e7b57996e03b11f05dc6aa852a839c1c8
Ariana Shannon <ashannon1229@gmail.com>
2026-07-14 19:06:59 -0500

    Changed TargetedBootstrapperTest to require that the library and DIA file are not null.

--- src/test/java/org/searlelab/contextguide/io/TargetedBootstrapperTest.java
+++ src/test/java/org/searlelab/contextguide/io/TargetedBootstrapperTest.java
@@ -14,16 +14,16 @@ import org.junit.rules.TemporaryFolder;
 public class TargetedBootstrapperTest {
 
 	@Rule
-	public TemporaryFolder tempFolder = new TemporaryFolder();
+	public TemporaryFolder tempFolder = TemporaryFolder.builder().assureDeletion().build();
 
 	@Test
 	public void testIfBootstrapperCreatesExpectedFiles() throws Throwable {
 		Path testDirectory = tempFolder.newFolder("targeted-bootstrapper-test").toPath();
 
-		URL libraryURL = getClass().getClassLoader().getResource("IL2_and_IL15_Combo.elib");
+		URL libraryURL = Objects.requireNonNull(getClass().getClassLoader().getResource("IL2_and_IL15_Combo.elib"));
 		String library = Paths.get(libraryURL.toURI()).toString();
 
-		URL diaURL = Objects.requireNonNull(getClass().getClassLoader().getResource("IL2A_GPFDIA_0combined.dia"));
+		URL diaURL = Objects.requireNonNull(getClass().getClassLoader().getResource("IL2A_GPFDIA_0combined_masked0_assay.dia"));
 		Path sourceDIAPath = Paths.get(diaURL.toURI());
 
 		Path testDIAPath = testDirectory.resolve("IL2A_GPFDIA_0combined.dia");
