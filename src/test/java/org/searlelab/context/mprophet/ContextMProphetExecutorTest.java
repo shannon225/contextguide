a489ca81e439acfd848c4183a36ca9ee31217a72
Ariana Shannon <ashannon1229@gmail.com>
2026-07-21 14:56:46 -0500

    Corrected the name of contextguide to context in org.searlelab.context.io and org.searlelab.context.mprophet

---
+++ src/test/java/org/searlelab/context/mprophet/ContextMProphetExecutorTest.java
@@ -0,0 +1,14 @@
+package org.searlelab.context.mprophet;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContextMProphetExecutorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
